a2dc3928a61af5bcbf39ee036b0e0efc2319c54f
Prathmesh Shinde <prathmeshshinde021@gmail.com>
2026-01-24 10:58:58 +0530

    React + Vite + MUI setup

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+const Login = () => {
+    return <Typography variant="h4">Login Page</Typography>;
+};
+
+export default Login;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+const Register = () => {
+    return <Typography variant="h4">Register Page</Typography>;
+};
+
+export default Register;
