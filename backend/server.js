50ad1b851bbd7cb1a02a619786934ca2f2f9ccea
Prathmesh Shinde <prathmeshshinde021@gmail.com>
2026-02-07 15:01:18 +0530

    Dockerfile for backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 
+
 if (process.env.USE_REDIS === "true") {
     require("./workers/movie.worker");
     console.log("Redis worker started");
@@ -19,3 +20,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ status: "OK" });
+});
+
